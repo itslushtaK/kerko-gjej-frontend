184291bd104f39ff989c5f91c97fc2d2afa0adec
Gentuar Lushtaku <93317180+itslushtaK@users.noreply.github.com>
2024-11-04 23:36:53 +0200

    Update AddLostItem.js

--- src/pages/AddLostItem.js
+++ src/pages/AddLostItem.js
@@ -52,7 +52,7 @@ const AddLostItem = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/lost-items/add",
+        "https://kerko-gjej-production.up.railway.app/api/lost-items/add",
         formData, // Changed from itemData to formData
         {
           headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
